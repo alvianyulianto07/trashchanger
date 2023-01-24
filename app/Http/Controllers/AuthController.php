00bcdb1339b35c73315f5930e74eed3ccfe71afd
Muhammad Luthfi <mluthfi3d@gmail.com>
2023-01-24 13:43:49 +0700

    register role

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,9 +1,9 @@
 <?php
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
@@ -32,7 +32,7 @@ class AuthController extends Controller
         }
         return back()->with('login_gagal', 'login gagal');
     }
-    
+
     public function logout(Request $request)
     {
         Auth::logout();
@@ -46,30 +46,30 @@ class AuthController extends Controller
 
     public function store(Request $request)
     {
-        $validate= $request->validate([
+        $validate = $request->validate([
             'name' => 'required',
             'email' => 'required|email|unique:users',
             'alamat' => 'required',
             'no_hp' => 'required',
             'password' => 'required|min:8',
-            'password_confirmation' => 'required|same:password'
+            'password_confirmation' => 'required|same:password',
         ]);
-        $role = 1;
-        if($request->check){
-            $role = 2;
+        $role = "1";
+        if ($request->check != null) {
+            $role = "2";
         }
         $user = User::create([
             'nama' => $request->name,
-            'email' =>  $request->email,
+            'email' => $request->email,
             'alamat' => $request->alamat,
             'no_hp' => $request->no_hp,
             'role' => $role,
             'password' => Hash::make($request->password),
         ]);
-        if($role == 1){
-            return redirect('/login')->with('success','Anda berhasil mendaftar');
-        } else {
-            return redirect('/daftarbanksampah')->with('success','Anda dapat melanjutkan mendaftar menjadi Bank Sampah');
+        if ($role == 1) {
+            return redirect('/login')->with('success', 'Anda berhasil mendaftar');
+        } else if ($role == 2) {
+            return redirect('/daftarbanksampah')->with('success', 'Anda dapat melanjutkan mendaftar menjadi Bank Sampah');
         }
     }
 
