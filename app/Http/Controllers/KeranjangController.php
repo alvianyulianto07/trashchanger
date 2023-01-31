c052067c7e979a3dd70f0dd031e2572f0a90697f
Muhammad Luthfi <mluthfi3d@gmail.com>
2023-01-31 08:46:26 +0700

    add keranjang controller

--- app/Http/Controllers/KeranjangController.php
+++ app/Http/Controllers/KeranjangController.php
@@ -1,8 +1,37 @@
 <?php
 namespace App\Http\Controllers;
 
+use App\Models\BankSampah;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class KeranjangController extends Controller
 {
+    
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $id = Auth::user()->id;
+        $keranjang = DB::table('keranjang')->where('users_id', $id)->get();
+        return view('pengepul.keranjang.index', compact('keranjang'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function checkout($id)
+    {
+        //
+        return view('pengepul.keranjang.checkout');
+    }
+    
 }
