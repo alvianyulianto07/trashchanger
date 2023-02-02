fdcc2c59474db5918e497c9786ba04261660d7d3
muhammadanjothosatru <muhammadanjothosatru@gmail.com>
2023-02-02 09:12:29 +0700

    keranjang ui

--- resources/views/pengepul/keranjang/index.blade.php
+++ resources/views/pengepul/keranjang/index.blade.php
@@ -6,37 +6,70 @@
             <div class="row">
                 <div class="col-9 m-0 p-0">
                     <div class="mb-3 p-3">
-                        <h3>Keranjang</h3>
+                        <h3 class="header-keranjang">Keranjang</h3>
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                            <label class="form-check-label" for="flexCheckDefault">
+                                Pilih Semua
+                            </label>
+                        </div>
+                        <hr>
                         @foreach ($cart as $nama_banksampah => $toko)
-                            <p class="toko-name">{{$nama_banksampah}}</p>
-                            @foreach ($toko as $item)
-                                <div class="item-keranjang mb-3">
-                                    <div class="row align-items-center">
-                                        <div class="col-1">
-                                            <div class="form-check">
-                                                <input class="form-check-input" type="checkbox" value=""
-                                                    id="flexCheckDefault">
-                                            </div>
-                                        </div>
-                                        <div class="col-3">
-                                            <img src="{{ asset('storage/foto/' . $item->foto) }}" class="card-img-top">
-                                        </div>
-                                        <div class="col-8">
-                                            <p class="trash-name">Jumlah: {{ $item->jumlah_barang }}</p>
-                                            <div class="m-2">
-                                                <p class="trash-name">Nama barang: {{ $item->nama_sampah }}</p>
-                                                <p class="cost">Harga satuan {{ $item->harga }}/kg</p>
-                                                <p class="bank-name">Bank Sampah:
-                                                    <strong>{{ $item->nama_banksampah }}</strong>
-                                                </p>
-                                                <p class="cost">{{ $item->total_harga }}</p>
-                                            </div>
-                                        </div>
+                            <div class="row align-items-center">
+                                <div class="col-1 d-flex justify-content-center">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value=""
+                                            id="flexCheckDefault">
                                     </div>
-
-
                                 </div>
-                            @endforeach
+                                <div class="col-11">
+                                    <div class="toko-name-keranjang">{{ $nama_banksampah }}</div>
+                                </div>
+                                @foreach ($toko as $item)
+                                    <div class="item-keranjang my-2">
+                                        <div class="row align-items-center mb-3">
+                                            <div class="col-1 d-flex justify-content-center">
+                                                <div class="form-check">
+                                                    <input class="form-check-input" type="checkbox" value=""
+                                                        id="flexCheckDefault">
+                                                </div>
+                                            </div>
+                                            <div class="col-3 text-center">
+                                                <img src="{{ asset('storage/foto/' . $item->foto) }}"
+                                                    class="card-img-keranjang">
+                                            </div>
+                                            <div class="col-8 p-0">
+                                                <div class="m-2">
+                                                    <p class="trash-name-keranjang">Nama barang: {{ $item->nama_sampah }}
+                                                    </p>
+                                                    <p class="cost-satuan-keranjang">Harga satuan {{ $item->harga }}/kg</p>
+                                                    <p class="cost-keranjang">{{ $item->total_harga }}</p>
+                                                </div>
+                                                <div class="d-flex justify-content-end">
+                                                    <div class="d-flex justify-content-end">
+                                                        <div class="btn btn-link px-2"
+                                                            onclick="this.parentNode.querySelector('input[type=number]').stepDown(); totalCost();">
+                                                            <i class="fas fa-minus"></i>
+                                                        </div>
+
+                                                        <input id="jumlah_barang" min="1" name="jumlah_barang"
+                                                            value="{{ $item->jumlah_barang }}" type="number"
+                                                            class="form-control form-control-barang-keranjang" />
+
+                                                        <div class="btn btn-link px-2"
+                                                            onclick="this.parentNode.querySelector('input[type=number]').stepUp(); totalCost();">
+                                                            <i class="fas fa-plus"></i>
+                                                        </div>
+                                                    </div>
+                                                    <button class="btn btn-sm btn-danger mx-3"><i class="far fa-trash-alt"
+                                                            style="margin-right: 5px"></i>Hapus</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>  
+                                @endforeach
+                            </div>
+                            <hr style="margin-top: 0">
                         @endforeach
                     </div>
                 </div>
@@ -55,11 +88,7 @@
                         <button class="btn btn-success btn-block mt-3">Beli (11)</button>
                     </div>
                 </div>
-
-
             </div>
-
-
         </div>
     </div>
 @endsection
