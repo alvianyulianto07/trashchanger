2f084ebf8d4bb52b4bb7add9e0a90104ea7a7abc
Muhammad Luthfi <mluthfi3d@gmail.com>
2023-02-06 13:42:01 +0700

    setengah perjalanan

--- app/Http/Controllers/PembelianController.php
+++ app/Http/Controllers/PembelianController.php
@@ -3,8 +3,10 @@ namespace App\Http\Controllers;
 
 use App\Models\Sampah;
 use App\Models\Kategori;
+use App\Models\Pembelian;
 use App\Models\BankSampah;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class PembelianController extends Controller
 {
@@ -21,8 +23,19 @@ class PembelianController extends Controller
         $searchquery = '';
         $banksampah = BankSampah::all();
         $sampah = Sampah::where('status', 'Tersedia')->get();
+
+        $id = Auth::user()->id;
+        $allpembelian = Pembelian::join('users', 'users.id', '=', 'pembelian.users_id')
+        ->join('transaksi', 'transaksi.pembelian_id', '=', 'pembelian.id')
+        ->join('bank_sampah', 'bank_sampah.id', '=', 'transaksi.bankSampah_id')
+        ->join('sampah', 'sampah.id', '=', 'transaksi.sampah_id')
+        ->join('kategori', 'kategori.id', '=', 'sampah.kategori_id')
+        ->where('pembelian.users_id', $id)
+        ->select('pembelian.id', 'bank_sampah.nama_banksampah', 'transaksi.status', 'sampah.nama_sampah')
+        ->get()
+        ->groupBy(['id', 'nama_banksampah']);
         $kategori = Kategori::all();
-        return view('pengepul.pembelian.index', compact('sampah', 'banksampah', 'kategori', 'searchquery'));
+        return view('pengepul.pembelian.index', compact('sampah', 'banksampah', 'kategori', 'searchquery', 'allpembelian'));
     }
 
     
--- resources/views/pengepul/pembelian/index.blade.php
+++ resources/views/pengepul/pembelian/index.blade.php
@@ -3,33 +3,40 @@
     <div class="container mt-3">
         <h3 class="header-keranjang mt-2">Daftar Transaksi</h3>
         <div class="card">
-            {{-- @foreach ($cart as $nama_banksampah => $toko) --}}
-            <div class="row align-items-center">
-                <div class="toko-name-pembelian">Kotakku</div>
-                {{-- @foreach ($toko as $item) --}}
-                <div class="row align-items-center mb-3">
-                    <div class="col-4 text-center">
-                        <img src="{{ asset('storage/foto') }}" class="card-img-pembelian">
-                    </div>
-                    <div class="col-8 p-0">
-                        <p class="status-pembelian">selesai</p>
-                        <p class="trash-name-keranjang">Nama barang:
-                            {{-- {{ $item->nama_sampah }} --}}
-                        </p>
-                        <p class="cost-satuan-keranjang">Harga satuan 5000/kg
-                        </p>
-                        <input value="10000" name="item" id="item" class="cost-keranjang" readonly />
-                        <p style="margin: 0">3 produk</p>
-                        <div class="d-flex justify-content-end">
-                            <button class="btn btn-sm btn-success mx-3"><i class="far fa-eye"
-                                    style="margin-right: 5px"></i>Lihat Transaksi</button>
-                        </div>
+            @foreach ($allpembelian as $pembelian)
+                <div>
+                    <div class="row align-items-center">
+                        @foreach ($pembelian as $nama_banksampah => $transaksi)
+                            <div class="toko-name-pembelian">{{ $nama_banksampah }}</div>
+                            @foreach ($transaksi as $sampah)
+                                @if ($loop->first)
+                                    <div class="row align-items-center mb-3">
+                                        <div class="col-4 text-center">
+                                            <img src="{{ asset('storage/foto') }}" class="card-img-pembelian">
+                                        </div>
+                                        <div class="col-8 p-0">
+                                            <p class="status-pembelian">{{ $sampah->status }}</p>
+                                            <p class="trash-name-keranjang">
+                                                {{-- {{ $pembelian->nama_sampah }} --}}
+                                            </p>
+                                            <p class="cost-satuan-keranjang">Harga satuan 5000/kg
+                                            </p>
+                                            <input value="10000" name="item" id="item" class="cost-keranjang"
+                                                readonly />
+                                            <p style="margin: 0">3 produk</p>
+                                            <div class="d-flex justify-content-end">
+                                                <button class="btn btn-sm btn-success mx-3"><i class="far fa-eye"
+                                                        style="margin-right: 5px"></i>Lihat Transaksi</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                @endif
+                            @endforeach
+                        @endforeach
                     </div>
                 </div>
-                </class=>
-                {{-- @endforeach --}}
-            </div>
-            {{-- @endforeach --}}
+                <hr>
+            @endforeach
         </div>
     </div>
 @endsection
