5865b24b3baac1a953f0e26857d3124024993f4e
alvianyulianto07 <alvianyulianto07@gmail.com>
2024-12-08 20:17:53 +0700

    ...

--- resources/views/banksampah/sampah/create.blade.php
+++ resources/views/banksampah/sampah/create.blade.php
@@ -5,7 +5,7 @@
             @csrf
             <div class="m-4">
                 <div class="row pl-0 pr-0">
-                    <div class="font-form-header mb-3 col-6">Masukkan Detail Kendaraan</div>
+                    <div class="font-form-header mb-3 col-6">Masukkan Detail Sampah</div>
                     <div class="font-form-header mb-3 col-6 d-flex justify-content-end">
                         <a href="{{ route('sampah.index') }}" class="btn btn-primary btn-sm"><i
                                 class="fas fa-arrow-left mr-2"></i>Kembali</a>
